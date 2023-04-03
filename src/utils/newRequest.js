1d27bf9be35c70aade6e412c88a2aa30c0201c4b
Madushanka Ishara <madushankaishara48@gmail.com>
2023-04-03 13:51:49 +0530

    reqfile change

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -4,8 +4,8 @@ const newRequest = axios.create({
   baseURL: "https://fiverr-testapi.onrender.com/api/",
   // baseURL: "http://localhost:8800/api/",
   withCredentials: true,
-  crossSite: "none",
-  crossDomain: true,
+  // crossSite: "none",
+  // crossDomain: true,
 });
 
 export default newRequest;
